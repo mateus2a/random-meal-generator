c75015ddc435af1a71f317e129f6d0fd7b1b6d55
Mateus <mtauws@gmail.com>
2022-05-30 13:13:59 -0300

    chore: create function getRandomMeal

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,13 +3,22 @@ import React from "react";
 import "./style.css";
 
 function Button() {
+  const getRandomMeal = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php").then((res) =>
+      res.json().then((data) => {
+        console.log(data);
+      })
+    );
+  };
   return (
     <div className="header-generator">
       <h2 className="title-generator">Felling hungry?</h2>
       <h4 className="subtitle-generator">
         Get a random meal by clicking below
       </h4>
-      <button type="submit">Get Meal</button>
+      <button type="submit" onClick={getRandomMeal}>
+        Get Meal
+      </button>
     </div>
   );
 }
