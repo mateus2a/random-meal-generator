e94d66fee08c1857e73858bce168fdf31b699013
Mateus <mtauws@gmail.com>
2022-05-30 11:05:11 -0300

    chore: create html of button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,6 +3,10 @@ import React from "react";
 function Button() {
   return (
     <div className="header-generator">
+      <h2 className="title-generator">Felling hungry?</h2>
+      <h4 className="subtitle-generator">
+        Get a random meal by clicking below
+      </h4>
       <button type="submit">Get Meal</button>
     </div>
   );
